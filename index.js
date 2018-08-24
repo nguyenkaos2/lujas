7e42e4986bfc6af67609b566c63f09655e99fed4
nguyenkaos2 <nguyen.cc.1990@gmail.com>
2018-08-24 15:49:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function kiusi(){
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   
   const page = await browser.newPage();
-  await page.goto('https://vaas.cf/asup/'); 
+  await page.goto('https://nguyenkaos.github.io/html/test2.html'); 
   console.log('----XONG0--------');
   setTimeout(async () => await await page.close(), 60000);
   setTimeout(async () => await browser.close(), 100000);
